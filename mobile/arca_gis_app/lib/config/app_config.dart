7cb54520a1f140b275fd3bfa80db1c17587bbb2e
MaximeKELI <presidentnetero01@gmail.com>
2026-06-11 23:19:23 +0000

    All coders

--- mobile/arca_gis_app/lib/config/app_config.dart
+++ mobile/arca_gis_app/lib/config/app_config.dart
@@ -3,8 +3,8 @@ class AppConfig {
   static const String appSubtitle = 'Agro-Rescue Climate Africa';
 
   // Android emulator: 10.0.2.2 maps to host localhost
-  static const String apiBaseUrl = 'http://10.0.2.2:8000/api';
-  static const String wsBaseUrl = 'ws://10.0.2.2:8000/ws/alerts/';
+  static const String apiBaseUrl = 'http://10.0.2.2:8002/api';
+  static const String wsBaseUrl = 'ws://10.0.2.2:8002/ws/alerts/';
 
   // For iOS simulator or physical device, use your machine IP:
   // static const String apiBaseUrl = 'http://192.168.1.X:8000/api';
