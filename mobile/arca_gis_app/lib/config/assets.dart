384f4c5bc6d5a88eb58085d5c3dcefd1c47373b7
MaximeKELI <presidentnetero01@gmail.com>
2026-06-12 02:48:11 +0000

    All coders

---
+++ mobile/arca_gis_app/lib/config/assets.dart
@@ -0,0 +1,8 @@
+/// Chemins des assets images (logo + carrousel).
+class AppAssets {
+  static const logo = 'assets/images/arca_logo.png';
+  static const carouselAgriculture = 'assets/images/carousel_agriculture.png';
+  static const carouselSos = 'assets/images/carousel_sos.png';
+  static const carouselClimate = 'assets/images/carousel_climate.png';
+  static const carouselCoop = 'assets/images/carousel_coop.png';
+}
--- mobile/arca_gis_app/lib/widgets/feature_carousel.dart
+++ mobile/arca_gis_app/lib/widgets/feature_carousel.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import '../config/app_config.dart';
 import 'arca_logo.dart';
 
 class CarouselSlide {
