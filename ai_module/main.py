41fae720257cec274b6820e939e12b2f49b1e92c
MaximeKELI <presidentnetero01@gmail.com>
2026-06-12 00:42:26 +0000

    All coders

--- ai_module/main.py
+++ ai_module/main.py
@@ -27,7 +27,7 @@ from yield_predictor import predict_yield
 app = FastAPI(
     title="ARCA-GIS AI Module",
     description="Analyse climatique, NDVI et recommandations agricoles pour l'Afrique",
-    version="5.0.0",
+    version="6.0.0",
 )
 
 
