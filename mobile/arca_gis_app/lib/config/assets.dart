bdbafe24dd0fc4379e4d632e36f0975adebad89e
MaximeKELI <presidentnetero01@gmail.com>
2026-06-12 03:10:38 +0000

    All coders

--- mobile/arca_gis_app/lib/config/assets.dart
+++ mobile/arca_gis_app/lib/config/assets.dart
@@ -5,4 +5,12 @@ class AppAssets {
   static const carouselSos = 'assets/images/carousel_sos.png';
   static const carouselClimate = 'assets/images/carousel_climate.png';
   static const carouselCoop = 'assets/images/carousel_coop.png';
+  static const carouselIa = 'assets/images/carousel_ia.png';
+  static const carouselAnalytics = 'assets/images/carousel_analytics.png';
+  static const carouselCarbon = 'assets/images/carousel_carbon.png';
+  static const carouselTraining = 'assets/images/carousel_training.png';
+  static const carouselWater = 'assets/images/carousel_water.png';
+  static const carouselMarket = 'assets/images/carousel_market.png';
+  static const carouselResilience = 'assets/images/carousel_resilience.png';
+  static const carouselLivestock = 'assets/images/carousel_livestock.png';
 }
